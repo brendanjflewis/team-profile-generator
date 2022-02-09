5ea768cfda55ca2815393a9549ab9c8b8e58529e
brendanjlewis <brendan.j.f.lewis@gmail.com>
2022-02-09 02:50:49 -0500

    added libs for team members

---
+++ index.js
@@ -0,0 +1,5 @@
+//team profiles
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
